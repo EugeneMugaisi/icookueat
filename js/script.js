f9b7a27803bbb6a228913fe6f989ef79507ba52a
EugeneMugaisi <mugaisieugene4@gmail.com>
2023-01-25 16:49:08 +0300

    completed frontend

--- js/script.js
+++ js/script.js
@@ -7,27 +7,51 @@ menu.onclick = () =>
     navbar.classList.toggle('active');
 }
 
-// window.onscroll = () =>
-// {
-//     menu.classList.remove('fa-times');
-//     navbar.classList.remove('active');
-// }
-
-// document.querySelector('#search-icon').onclick = () =>
-// {
-//     document.querySelector('#search-form').classList.toggle(active);
-// }
-
-// document.querySelector('#close').onclick = () =>
-// {
-//     document.querySelector('#search-form').classList.remove(active);
-// }
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
 
 window.onscroll = () =>
 {
+
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-}
+
+    // section.forEach(sec =>{
+
+    //   let top = window.scrollY;
+    //   let height = sec.offsetHeight;
+    //   let offset = sec.offsetTop - 150;
+    //   let id = sec.getAttribute('id');
+
+    //   if(top => offset && top < offset + height){
+    //     navLinks.forEach(links =>{
+    //       links.classList.remove('active');
+    //       document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+    //     });
+    //   };
+
+    // });
+
+    // section.forEach (sec =>
+    // {
+
+    //   let top = window.scrollY;
+    //   let height = sec.offsetHeight;
+    //   let offset = sec.offsetTop - 150;
+    //   let id = sec.getAttribute('id');
+
+    //   if(top => offset && top < offset + height)
+    //   {
+    //     navLinks.forEach(links => 
+    //     {
+    //       links.classList.remove('active');
+    //       document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+    //     });
+    //   };
+
+    // });
+    
+};
 
 document.querySelector('#search-icon').onclick = () =>{
     document.querySelector('#search-form').classList.toggle('active');
@@ -74,3 +98,13 @@ var swiper = new Swiper(".home-slider", {
       },
     },
   });
+
+  function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+
+  function fadeOut(){
+    setInterval(loader, 1);
+  }
+
+  window.onload = fadeOut;
